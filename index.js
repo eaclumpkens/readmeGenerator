5ddd213b0c24331ce86821f9aef260d42f5c779c
Elisabeth Clumpkens <eaclumpkens@gmail.com>
2020-10-15 23:42:17 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,77 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+const appendFileAsync = util.promisify(fs.appendFile);
+
+function generateReadme(response) {
+    return `
+    
+# ${response.projectTitle}
+
+${response.description}
+
+### Contributors
+
+${response.contributors}
+
+----
+
+### Contact Me
+
+GitHub: ${response.github}
+${response.email}
+`
+    }
+
+inquirer.prompt([
+    {
+        type: "input",
+        message: "Project Title:",
+        name: "projectTitle"
+    },
+    {
+        type: "input",
+        message: "Project Description:",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Installations:",
+        name: "installations"
+    },
+    {
+        type: "list",
+        message: "License:",
+        name: "license",
+        choices: [
+            "GNU",
+            "MIT",
+            "ISC",
+            "Apache"
+        ]
+    },
+    {
+        type: "input",
+        message: "Contributors:",
+        name: "contributors",
+    },
+    {
+        type: "input",
+        message: "GitHub Username:",
+        name: "github",
+    },
+    {
+        type: "input",
+        message: "Email: ",
+        name: "email",
+    }
+]).then(function(data) {
+    console.log("Success.");
+
+    appendFileAsync('README.md', generateReadme(data))
+
+}).catch(function(err) {
+    console.log(err);
+})
